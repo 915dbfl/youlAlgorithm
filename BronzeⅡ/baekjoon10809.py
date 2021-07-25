01e42fb3150f4356c9bf62b4acf5ad56bc69ea24
915dbfl <dbfl00915@naver.com>
2021-07-25 14:59:51 +0900

    21.07.23 알고리즘

--- BronzeⅡ/baekjoon10809.py
+++ BronzeⅡ/baekjoon10809.py
@@ -1,11 +1,4 @@
-#21.07.23
+# #21.07.23
 word = input()
-lst = ["-1" for i in range(26)]
-count = 0
-for i in word:
-  #ord, chr 정리하기
-  if lst[ord(i) - 97] == "-1":
-    lst[ord(i) - 97] = str(count)
-  count += 1
-#.join 함수 정리하기
-print(" ".join(lst))
+for i in range(97, 123): 
+  print(word.find(chr(i)), end= " ")
