2f2fff42e1092bae0c72d0ecd66b4c600eefbbb6
915dbfl <dbfl00915@naver.com>
2021-08-26 21:16:56 +0900

    21.08.26 알고리즘

--- SilverⅤ/baekjoon1010.py
+++ SilverⅤ/baekjoon1010.py
@@ -1,20 +1,7 @@
 #21.08.24
-import sys
+import sys, math
 num = int(input())
-cases = []
 for i in range(num):
-  cases.append(list(map(int, sys.stdin.readline().rstrip().split())))
-for n, m in cases:
-  dif = m-n+1
-  lst = []
-  if n ==1:
-    print(m)
-  elif n == m:
-    print(1)
-  else:
-    for i in range(n-2+1):
-      if i == 0:
-        lst = [i for i in range(1, dif+1)]
-      else:
-        lst = [sum(lst[0:i+1]) for i in range(0, dif) if lst]
-    print(sum(lst))
+  n, m = map(int, sys.stdin.readline().rstrip().split())
+  case = math.factorial(m) // (math.factorial(n) * math.factorial(m-n))
+  print(case)
---
+++ SilverⅤ/baekjoon10610.py
@@ -0,0 +1,8 @@
+#21.08.26
+lst = list(map(int, input()))
+lst.sort(reverse=True)
+if lst[-1] != 0 or sum(lst) % 3 != 0:
+  print(-1)
+else:
+  for i in lst:
+    print(i, end="")
