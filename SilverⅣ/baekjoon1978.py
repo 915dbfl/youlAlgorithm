f3a6313c87af2301102c20e6f0cf02c04e886706
915dbfl <dbfl00915@naver.com>
2021-09-02 21:51:58 +0900

    21.09.02 알고리즘(에라토스테네스의 체)

--- SilverⅣ/baekjoon1978.py
+++ SilverⅣ/baekjoon1978.py
@@ -1,12 +1,17 @@
 #21.09.01
+#소수 찾기: 에라토스테네스의 체 사용
 num = int(input())
 nums = list(map(int, input().split()))
+end = max(nums)
+count = 0
+lst = [i for i in range(2, end + 1)]
+for i in range(2, int(end ** 0.5)+1):
+  if i in lst:
+    for j in range(i*2, end+1, i):
+      if j in lst:
+        lst.remove(j)
+
 for i in nums:
-  if i == 1:
-    num -= 1
-  elif i != 2:
-    for j in range(2,int(i**0.5)+1):
-      if i % j == 0:
-        num -= 1
-        break
-print(num)
+  if i in lst:
+    count+= 1
+print(count)
