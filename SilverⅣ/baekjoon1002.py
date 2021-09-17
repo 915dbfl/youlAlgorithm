eba96011c0e1de7056cea9d96f58fcc872a684c1
915dbfl <dbfl00915@naver.com>
2021-09-17 15:44:06 +0900

    21.09.17 터렛 알고리즘 수정

--- SilverⅣ/baekjoon1002.py
+++ SilverⅣ/baekjoon1002.py
@@ -1,25 +1,21 @@
 #21.09.05
 n = int(input())
 for i in range(n):
-  test = list(map(int, input().split()))
-  if test[0] == test[3] and test[1] == test[4]:
-    if test[2] == test[5]:
-      print(-1)
-    else:
-      print(0)
+  x1, y1, r1, x2, y2, r2 = list(map(int, input().split()))
+  l = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+  r = [r1, r2, l]
+  m = max(r)
+  r.remove(m)
+
+  # 중심이 동일하고 반지름까지 같을 경우
+  if (l == 0 and r1 == r2):
+    print(-1)
+  # 외접이거나 내접일 경우
+  elif (l == r1 + r2 or m ==sum(r)):
+    print(1)
+  # 한 원 내부에 다른 원이 닿지 않고 존재하거나, 두 원이 완전히 다른 위치에 있어 겹치지 않는 경우
+  elif (m > sum(r)):
+    print(0)
+  # 두 원이 두 점에서 만나는 경우
   else:
-    r1 = ((test[3] - test[0])**2 + (test[4] - test[1])**2)**0.5
-    r2 = test[2] + test[5]
-    if r1 < r2:
-      lst = [test[2], test[5]]
-      lst.sort()
-      if r1 + lst[0] == lst[1]:
-        print(1)
-      elif r1 + lst[0] < lst[1]:
-        print(0)
-      elif r1 + lst[0] > lst[1]:
-        print(2)
-    elif r1 == r2:
-      print(1)
-    elif r1 > r2:
-      print(0)
+    print(2)
