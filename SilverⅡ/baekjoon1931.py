c0ae6997e257a93b552298de3ab7e66540578551
915dbfl <dbfl00915@naver.com>
2021-11-16 11:53:23 +0900

    21.11.16 1931 알고리즘 수정

--- SilverⅡ/baekjoon1931.py
+++ SilverⅡ/baekjoon1931.py
@@ -1,27 +1,18 @@
-#21.11.15
+#21.11.16
 #그리디
 import sys
 N = int(sys.stdin.readline().rstrip())
 lst = []
-s = 0
 count = 0
-for i in range(11):
+for i in range(N):
   lst.append(list(map(int, sys.stdin.readline().rstrip().split())))
+lst.sort(key= lambda i : (i[1], i[0]))
 
-lst.sort(key= lambda i : i[0])
-
-while 1:
-  if(s == N-1):
+k, l = lst[0]
+for j in range(1, N):
+  n, o = lst[j]
+  if n >= l:
     count += 1
-    break
-  k, l = lst[s]
-  for j in range(s+1, N):
-    n, o = lst[j]
-    if o <= l:
-      k, l = n, o
-    elif n >= l:
-      s = j
-      count += 1
-      break
+    k, l = n, o
 
-print(count)
+print(count + 1)
