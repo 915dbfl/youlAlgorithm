75e16c3c30d281946ab81c00350cbd2f835a7a66
915dbfl <dbfl00915@naver.com>
2022-01-23 18:18:47 +0900

    11279 알고리즘 수정

--- SilverⅡ/baekjoon11279🔔.py
+++ SilverⅡ/baekjoon11279🔔.py
@@ -1,53 +1,18 @@
-#21.01.18
+#21.01.19
 #11279: 최대 힙
 import sys
+import heapq
+
 N = int(input())
-heap = [0]
+heap = []
 
-def add(x):
-  heap.append(x)
-  idx = len(heap)-1
-  while 1:
-    if idx == 1 or x < heap[idx//2]:
-      break
-    else:
-      heap[idx] = heap[idx//2]
-      heap[idx//2] = x
-      idx //= 2
-
-def delete():
-  print(heap[1])
-  lst = heap[-1]
-  heap[1] = lst
-  del(heap[-1])
-  idx = 1
-  while 1:
-    if len(heap) < idx*2 + 1:
-      break
-    elif len(heap) >= idx*2 + 2:
-      if heap[idx*2] > heap[idx*2 + 1]:
-        max = idx*2
-      else:
-        max = idx*2 + 1
-      if lst < heap[max]:
-        heap[idx] = heap[max]
-        heap[max] = lst
-        idx = max
-      else:
-        break
-    elif lst < heap[idx*2 ]:
-      heap[idx] = heap[idx*2]
-      heap[idx*2] = lst
-      idx = idx * 2
-    else:
-      break
-
-for i in range(N):
+for _ in range(N):
   x = int(sys.stdin.readline())
   if x == 0:
-    if len(heap) == 1:
+    try:
+      print(-1 * heapq.heappop(heap))
+    except:
       print(0)
-    else:
-      delete()
+
   else:
-    add(x)
+    heapq.heappush(heap, -x)
