c8aed16e5e6665df124dfadb128952cee34d53dd
915dbfl <dbfl00915@naver.com>
2022-03-25 13:32:55 +0900

    2022.03.25 알고리즘

---
+++ Programmers/level2/phone_number.py
@@ -0,0 +1,10 @@
+#2022.03.25
+#전화번호 목록
+
+#best 풀이
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] in phone_book[i+1][0:len(phone_book[i])]:
+            return False
+    return True
