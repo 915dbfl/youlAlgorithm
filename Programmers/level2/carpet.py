80ff0462de0025a620947c7da8978eba44415a39
915dbfl <dbfl00915@naver.com>
2022-04-12 20:58:51 +0900

    22.04.11 알고리즘 풀이 추가

--- Programmers/level2/carpet.py
+++ Programmers/level2/carpet.py
@@ -2,9 +2,16 @@
 #카펫
 
 #내 풀이
-
 def solution(brown, yellow):
     plus = (brown + 4)//2
     for y in range(1, plus//2+1):
         if y * (plus - y) == brown + yellow:
             return [plus-y, y]
+
+#근의 공식 사용하기
+# xy = brown + yellow
+# (x-2)(y-2) = yellow
+def solution(brown, yellow):
+    a = (brown + 4 + ((brown + 4) ** 2 - 16 * (brown + yellow)) ** 0.5) / 4
+    b = (brown + yellow) / a
+    return [max(a,b), min(a,b)]
