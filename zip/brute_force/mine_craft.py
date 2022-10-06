188375a38b9088a76ff6f3ad1c55463476991235
915dbfl <dbfl00915@naver.com>
2022-10-06 16:06:48 +0900

    마인크래프트 알고리즘 수정

--- zip/brute_force/mine_craft.py
+++ zip/brute_force/mine_craft.py
@@ -6,31 +6,29 @@
 from collections import Counter
 import sys
 
-n, m, b = map(int, sys.stdin.readline().split())
+n, m, inv = map(int, sys.stdin.readline().split())
 ground = []
 
-for i in range(n):
-  ground += list(map(int, sys.stdin.readline().split()))
+for _ in range(n):
+  ground += map(int, sys.stdin.readline().split())
 
-counter = Counter(ground).most_common()
+_sum = sum(ground)
+h_min = min(ground)
+h_max = max(ground)
+ground = dict(Counter(ground))
 
-if len(counter) == 1:
-  print(0, counter[0][0])
-else:
-  answer = [n*m*2*256, 0]
+time, height = 0, 0
 
-  for i in range(max(ground), min(ground)-1, -1):
-    s = 0
-    inventory = b
-    m, r = 0, 0
-    for v, c in counter:
-      if i < v:
-        s += 2*c*(v-i)
-        m += c*(v-i)
-      elif i > v:
-        s += c*(i-v)
-        r += c*(i-v)
-    if inventory + m - r >= 0 and s < answer[0]:
-      answer = [s, i]
+for h in range(h_min, h_max+1):
+  if _sum + inv >= n*m*h:
+    t = 0
+    for key in ground:
+      if h < key:
+        t += 2*ground[key]*(key-h)
+      elif h > key:
+        t += ground[key]*(h-key)
+    if t <= time or time == 0:
+      time = t
+      height = h
 
-  print(answer[0], answer[1])
+print(time, height)
