41c5474b0a6b3e386261e8b091fd269543af5502
915dbfl <dbfl00915@naver.com>
2022-10-11 21:37:47 +0900

    22.10.11 알고리즘

--- zip/divide_and_conquer/z.py
+++ zip/divide_and_conquer/z.py
@@ -1,7 +1,9 @@
 #22.10.09
 #z
 #class3/실버1
+#분할 정복/재귀
 
+#내 풀이: 분할정복
 n, r, c = map(int, input().split())
 
 answer = 0
@@ -15,3 +17,36 @@ while n > 0:
   
   n -= 1
 print(answer)
+
+#분할정복-1
+n, r, c = map(int, input().split())
+
+answer = 0
+
+while n > 0:
+  if r < 2**(n-1) and c < 2**(n-1): #1사분면
+    answer += 2**(n-1) * 2**(n-1) * 0
+  elif r < 2**(n-1) and c >= 2**(n-1): #2사분면
+    answer += 2**(n-1) * 2**(n-1) * 1
+    c -= 2**(n-1)
+  elif r >= 2**(n-1) and c < 2**(n-1):#3사분면
+    answer += 2**(n-1) * 2**(n-1) * 2
+    r -= 2**(n-1)
+  elif r >= 2**(n-1) and c >= 2**(n-1):
+    answer += 2**(n-1) * 2**(n-1) * 3
+    r -= 2**(n-1)
+    c -= 2**(n-1)
+
+  n -= 1
+
+print(answer)
+
+#재귀
+def getAnswer(N, r, c):
+  if N == 0:
+    return 0
+  return 2*(r%2)+(c%2) + 4 * getAnswer(N-1, r//2, c//2)
+
+n, r, c = map(int, input().split())
+
+print(getAnswer(n, r, c))
