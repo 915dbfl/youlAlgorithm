cd8e01c3c856fd27fb4338e6fdef48c7509cd8fe
915dbfl <dbfl00915@naver.com>
2022-11-30 17:00:47 +0900

    스티커_22.11.30 dp 리팩토링

--- zip/dp/sticker.py
+++ zip/dp/sticker.py
@@ -59,10 +59,10 @@ for _ in range(t):
 
     while x < n:
       if y == 0:
-        sticker[y][x] += max(sticker[1][x-1], sticker[1][x-2], sticker[0][x-2])
+        sticker[y][x] += max(sticker[1][x-1], sticker[1][x-2])
         y = 1
       else:
-        sticker[y][x] += max(sticker[0][x-1], sticker[0][x-2], sticker[1][x-2])
+        sticker[y][x] += max(sticker[0][x-1], sticker[0][x-2])
         y, x = 0, x+1
 
   print(max(sticker[0][-1], sticker[1][-1]))
