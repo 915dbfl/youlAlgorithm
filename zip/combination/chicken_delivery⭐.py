3edb200dbc69f0fdf677a30671b8c79b48b21a57
915dbfl <dbfl00915@naver.com>
2022-12-19 23:32:45 +0900

    치킨 배달__combinations 사용

--- zip/combination/chicken_delivery⭐.py
+++ zip/combination/chicken_delivery⭐.py
@@ -2,8 +2,8 @@
 #치킨 배달
 #class4/골드5
 
-#그리디 알고리즘: 오답
-#그리디를 통해 남긴 치킨 집이 최선의 결과가 아니다.
+# 그리디 알고리즘: 오답
+# 그리디를 통해 남긴 치킨 집이 최선의 결과가 아니다.
 import sys
 from collections import defaultdict
 input = sys.stdin.readline
@@ -26,6 +26,7 @@ for c in chicken:
   for h in house:
     dis = abs(c[0]-h[0]) + abs(c[1]-h[1])
     chicken_dis[c].append(dis)
+print(chicken_dis)
 
 if len(chicken) != m:
   lst = sorted(chicken_dis.keys(), key = lambda x: sum(chicken_dis[x]))[:m]
@@ -37,5 +38,34 @@ else:
 answer = 0
 for c in zip(*lst):
   answer += min(c)
+
+print(answer)
+
+# 조합 사용
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+city = list(list(map(int, input().split())) for _ in range(n))
+chicken = []
+house = []
+
+for i in range(n):
+  for j in range(n):
+    if city[i][j] == 1:
+      house.append((i, j))
+    elif city[i][j] == 2:
+      chicken.append((i, j))
+
+answer = 2*n*len(house)
+for case in combinations(chicken, m):
+  tmp = 0
+  for h in house:
+    tmp += min([abs(h[0]-c[0])+abs(h[1]-c[1]) for c in case])
+    if tmp >= answer:
+      break
+
+  answer = min(answer, tmp)
 
 print(answer)
