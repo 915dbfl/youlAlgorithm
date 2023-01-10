ce65ad7bba77ca4f7c3e50078ccf36b67ed2034c
915dbfl <dbfl00915@naver.com>
2023-01-10 09:37:04 +0900

    AC_23.01.10 재풀이

--- zip/stack(queue)/ac.py
+++ zip/stack(queue)/ac.py
@@ -3,42 +3,71 @@
 #class3/골드5
 #queue
 
-
 # 'R'에서 revrse를 할 경우, 시간초과가 발생! -> O(N)
 # flag를 사용하여 reverse는 마지막 한 번만 실행하도록 한다.
+#시간초과
 import sys
 from collections import deque
 input = sys.stdin.readline
 
-def getAnswer(dq, cmd, back):
-  for i in cmd:
-    if i == "R":
-      back = (back+1)%2
+t = int(input())
+
+for _ in range(t):
+  funcs = input().rstrip()
+
+  n = int(input())
+  if n == 0:
+    dq = deque(input().rstrip()[1:1])
+  else:
+    dq = deque(input().rstrip()[1:n*2].split(","))
+
+  for c in funcs:
+    if c == "D":
+      if len(dq) > 0:
+        dq.popleft()
+      else:
+        print("error")
+        break
     else:
-      if len(dq) != 0:
-        if back == 1:
+      dq = deque(list(dq)[::-1])
+  else:
+    print("[")
+    print(",".join(dq), end = "")
+    print("]")
+
+# 리팩토링
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+  funcs = input().rstrip()
+  check_d = 0
+
+  n = int(input())
+  if n == 0:
+    dq = deque(input().rstrip()[1:-1])
+  else:
+    dq = deque(input().rstrip()[1:-1].split(","))
+
+  for c in funcs:
+    if c == "D":
+      if len(dq) > 0:
+        if check_d:
           dq.pop()
         else:
           dq.popleft()
       else:
         print("error")
         break
-  else:
-    print("[", end="")
-    if back == 1:
-      print(",".join(list(dq)[::-1]), end="")
     else:
-      print(",".join(list(dq)), end="")
-    print("]")
-
-t = int(input())
-
-for _ in range(t):
-  cmd = input().rstrip()
-  n = int(input())
-  lst = input().rstrip()
-  if n == 0:
-    dq = deque()
+      check_d = abs(check_d-1)
   else:
-    dq = deque(list(lst[1:-1].split(",")))
-  getAnswer(dq, cmd, 0)
+    if check_d:
+      dq = list(dq)[::-1]
+
+    print("[", end= "")
+    print(",".join(dq), end = "")
+    print("]")
