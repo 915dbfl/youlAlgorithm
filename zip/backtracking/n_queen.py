f327ff3c21a4567b75ffee26f92a7e0880b194f7
915dbfl <dbfl00915@naver.com>
2023-01-13 23:01:48 +0900

    N-Queen 풀이 추가

--- zip/backtracking/n_queen.py
+++ zip/backtracking/n_queen.py
@@ -31,3 +31,30 @@ visited = [-1 for _ in range(n)]
 setQueen(0)
 
 print(answer)
+
+# 다른 풀이
+n = int(input())
+
+def check(i):
+  for k in range(0, i):
+    if visited[i] == visited[k] or abs(visited[i] - visited[k]) == abs(i-k):
+      return False
+  return True
+
+def setQueen(i):
+  global answer
+  if i == n:
+    answer += 1
+    return
+
+  for j in range(n):
+    visited[i] = j
+    if check(i):
+      setQueen(i+1)
+
+answer = 0
+visited = [0 for _ in range(n)]
+
+setQueen(0)
+
+print(answer)
